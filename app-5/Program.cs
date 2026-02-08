044065d03209d044566e006bbf7cf649fe93f2d3
mariamboules9876-blip <mariamboules9876@gmail.com>
2026-02-08 08:04:13 -0800

    Part1

--- app-5/Program.cs
+++ app-5/Program.cs
@@ -4,7 +4,54 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            //part1
+
+
+
+
+//            using System;
+
+//enum DayOfWeekEnum
+//        {
+//            Saturday,
+//            Sunday,
+//            Monday,
+//            Tuesday,
+//            Wednesday,
+//            Thursday,
+//            Friday
+//        }
+
+//        class Program
+//        {
+//            static void Main()
+//            {
+//                Console.Write("Enter a day number (0-6): ");
+//                int number = int.Parse(Console.ReadLine());
+
+//                if (number < 0 || number > 6)
+//                {
+//                    Console.WriteLine("Invalid input");
+//                    return;
+//                }
+
+//                DayOfWeekEnum day = (DayOfWeekEnum)number;
+//                Console.WriteLine("Day: " + day);
+
+//                switch (day)
+//                {
+//                    case DayOfWeekEnum.Saturday:
+//                    case DayOfWeekEnum.Friday:
+//                        Console.WriteLine("It's the Weekend");
+//                        break;
+
+//                    default:
+//                        Console.WriteLine("It's a Workday");
+//                        break;
+//                }
+//            }
+//        }
+
     }
+}
 }
