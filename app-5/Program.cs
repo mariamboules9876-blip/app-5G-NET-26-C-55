1434a203b182b5022f9e47d5a29d52a3c3d12148
mariamboules9876-blip <mariamboules9876@gmail.com>
2026-02-08 08:05:22 -0800

    Part2-Q1

--- app-5/Program.cs
+++ app-5/Program.cs
@@ -9,48 +9,101 @@
 
 
 
+            //            using System;
+
+            //enum DayOfWeekEnum
+            //        {
+            //            Saturday,
+            //            Sunday,
+            //            Monday,
+            //            Tuesday,
+            //            Wednesday,
+            //            Thursday,
+            //            Friday
+            //        }
+
+            //        class Program
+            //        {
+            //            static void Main()
+            //            {
+            //                Console.Write("Enter a day number (0-6): ");
+            //                int number = int.Parse(Console.ReadLine());
+
+            //                if (number < 0 || number > 6)
+            //                {
+            //                    Console.WriteLine("Invalid input");
+            //                    return;
+            //                }
+
+            //                DayOfWeekEnum day = (DayOfWeekEnum)number;
+            //                Console.WriteLine("Day: " + day);
+
+            //                switch (day)
+            //                {
+            //                    case DayOfWeekEnum.Saturday:
+            //                    case DayOfWeekEnum.Friday:
+            //                        Console.WriteLine("It's the Weekend");
+            //                        break;
+
+            //                    default:
+            //                        Console.WriteLine("It's a Workday");
+            //                        break;
+            //                }
+            //            }
+            //        }
+
+
+
+
+            //Part2-Q1
+
+
 //            using System;
 
-//enum DayOfWeekEnum
-//        {
-//            Saturday,
-//            Sunday,
-//            Monday,
-//            Tuesday,
-//            Wednesday,
-//            Thursday,
-//            Friday
-//        }
-
-//        class Program
+//class Program
 //        {
 //            static void Main()
 //            {
-//                Console.Write("Enter a day number (0-6): ");
-//                int number = int.Parse(Console.ReadLine());
+//                Console.Write("Enter array size: ");
+//                int size = int.Parse(Console.ReadLine());
 
-//                if (number < 0 || number > 6)
+//                int[] numbers = new int[size];
+//                int sum = 0;
+
+//                for (int i = 0; i < size; i++)
 //                {
-//                    Console.WriteLine("Invalid input");
-//                    return;
+//                    Console.Write($"Enter element [{i}]: ");
+//                    numbers[i] = int.Parse(Console.ReadLine());
+//                    sum += numbers[i];
 //                }
 
-//                DayOfWeekEnum day = (DayOfWeekEnum)number;
-//                Console.WriteLine("Day: " + day);
+//                int max = numbers[0];
+//                int min = numbers[0];
 
-//                switch (day)
+//                for (int i = 1; i < size; i++)
 //                {
-//                    case DayOfWeekEnum.Saturday:
-//                    case DayOfWeekEnum.Friday:
-//                        Console.WriteLine("It's the Weekend");
-//                        break;
+//                    if (numbers[i] > max)
+//                        max = numbers[i];
 
-//                    default:
-//                        Console.WriteLine("It's a Workday");
-//                        break;
+//                    if (numbers[i] < min)
+//                        min = numbers[i];
+//                }
+
+//                double average = (double)sum / size;
+
+//                Console.WriteLine("Sum = " + sum);
+//                Console.WriteLine("Average = " + average);
+//                Console.WriteLine("Max = " + max);
+//                Console.WriteLine("Min = " + min);
+
+//                Console.Write("Reverse = ");
+//                for (int i = size - 1; i >= 0; i--)
+//                {
+//                    Console.Write(numbers[i] + " ");
 //                }
 //            }
 //        }
+
 
     }
 }
